ab9d3a5e2dbadf624be5811e5a0c870b57c24cb1
laura <jvestudiante912@gmail.com>
2022-03-24 21:05:10 -0500

    actualizar4
    actualizar4

---
+++ ejercicio de repado 2 capitulo 4/edad.py
@@ -0,0 +1,10 @@
+edad=int(input("ingrese su edad:  "))
+peso=int(input("ingrese su peso:  "))
+heartbeat=int(input("ingrese su heartbeat:  "))
+plaquetas=int(input("digite sus plaquetas:  "))
+if edad==34  and peso==81  and heartbeat==70 and plaquetas==150000:
+    print("apto para donar sangre  ")
+
+else: 
+    print("no apto para donar sandre ")
+
