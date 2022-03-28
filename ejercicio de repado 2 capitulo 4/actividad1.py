4d91cdfd580d179e6347d553860f9bce76fbc8a6
Laura12121 <101460406+Laura12121@users.noreply.github.com>
2022-03-28 18:37:51 -0500

    ACTUALIZAR5
    ACTUALIZAR5

---
+++ ejercicio de repado 2 capitulo 4/actividad1.py
@@ -0,0 +1,10 @@
+tecla_1=input("digite opcion tecla 1:  ")
+tecla_2=input("digite opcion tecla 2:  ")
+tecla_3=input("digite opcion tecla 3:  ")
+
+if tecla_1=="ctrl" and tecla_2=="Alt"and tecla_3=="Del":
+    print("cerrar secion")
+else:
+    print("no cerrar secion")
+    
+    
---
+++ ejercicio de repado 2 capitulo 4/bandera.py
@@ -0,0 +1,13 @@
+pais=(input("escriba el nombre del pais:  "))
+if pais=="italia":
+    bandera=("💙")
+    print(f"bandera:  {bandera}   ")
+elif pais=="brasil":
+    bandera=("💛")
+    print(f"bandera:  {bandera}   ")
+elif pais=="colombia":
+    bandera=("❤")
+    print(f"bandera:  {bandera} ")
+else:
+    print(f"la bandera no se encontro")
+    
